1895de57985c42bf6e4278bfa402e413746f9ae2
Parry Maxime <maximegp@hotmail.fr>
2023-03-13 15:21:01 +0100

    mdMax

--- src/app/services/participant.service.ts
+++ src/app/services/participant.service.ts
@@ -37,29 +37,21 @@ export class ParticipantService {
 
 
 
-<<<<<<< HEAD
-=======
+
+
   addParticipant(f:Participant)
   {
     return this.http.post("http://localhost:8015/api/participants", f)
   }
 
 
-  modifPersonneParticipant(id:number)
-  {
-    return this.http.post(`http://localhost:8015/api/modifierPersonneParticipants/${id}`, id)
-  }
-
-  getParticipantById(id:number)
-  {
-    return this.http.get<Participant>(`http://localhost:8015/api/participants/${id}`)
-  }
+  
+ 
 
   modifPersonneUtilisateur(id:number)
   {
     return this.http.post(`http://localhost:8015/api/modifierPersonneUtilisateurs/${id}`, id)
   }
 
->>>>>>> main
 
 }
